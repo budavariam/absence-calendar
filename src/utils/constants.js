6492253e3e0d52022150711d8679d5a73a606f86
Mátyás Budavári <budavariam@gmail.com>
2022-07-15 19:05:53 +0200

    Select builtin members by default

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,18 +16,23 @@ function generateDate(addToCurrentMonth = 0, setDay = 1) {
   return generatedDate.toISOString().split('T')[0]
 }
 
+const defaultMembers = [
+  "John Doe",
+  "Jane Doe"
+]
+
 export const LOCALSTORAGE_DEFAULT = {
     "RAWEVENTDATA": `[
         {
-          "who": "John Doe",
+          "who": "${defaultMembers[0]}",
           "start": "${generateDate(0, 25)}",
           "end": "${generateDate(1, 3)}"
         },
         {
-          "who": "Jane Doe",
+          "who": "${defaultMembers[1]}",
           "start": "${generateDate(1, 2)}",
           "end": "${generateDate(1, 13)}"
         }
       ]`,
-    "SELECTEDMEMBERS": "[]",
+    "SELECTEDMEMBERS": JSON.stringify(defaultMembers),
 }
